3070506d07a21191bf6ded9e1c447ec757664845
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-24 16:59:40 -0700

    adding user account stuff

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 using PierresTreats.Models;
 
@@ -11,11 +13,21 @@ namespace PierresTreats.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly PierresTreatsContext _db;
+      private readonly UserManager<ApplicationUser> _userManager;
+
+      public HomeController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+      {
+        _userManager = userManager;
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
         return View();
       }
+
+      
     }
 }
