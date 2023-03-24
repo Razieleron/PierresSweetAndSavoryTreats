edd9414b49ac05a805c46d1a869994be76de4aa9
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-24 14:53:57 -0700

    add wwwroot folder and css/img subfolders

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -20,21 +20,19 @@ namespace PierresTreats.Controllers
 
     public ActionResult Index()
     {
-      return View(_db.Recipes.ToList());
+      return View(_db.Treats.ToList());
+    }
+      public ActionResult Create()
+    {
+      return View ();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
-
-  public ActionResult Create()
-  {
-    return View ();
-  }
-
-  [HttpPost]
-  public ActionResult Create(Treat treat)
-  {
-    _db.Treats.Add(treat);
-    _db.SaveChanges();
-    return RedirectToAction("Index");
-  }
-  
 }
