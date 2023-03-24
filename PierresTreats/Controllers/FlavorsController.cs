fd2c9f8114eb24f1c8e85e493028c140c50b95f5
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-24 15:01:59 -0700

    add logic for index and create to FlavorsController.cs

---
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,38 @@
+using System.Net.Mail;
+using System.Reflection.PortableExecutable;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+using PierresTreats.Models;
+
+namespace PierresTreats.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    public FlavorsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList());
+    }
+      public ActionResult Create()
+    {
+      return View ();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
