fd1544fcdd5f757f7014b60d396ddf5427784c4d
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-24 15:40:49 -0700

    added linking from treats to flavors in cshtml and in TreatsController.cs

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -50,12 +50,35 @@ namespace PierresTreats.Controllers
       return View(thisTreat);
     }
 
+
+
     [HttpPost]
     public ActionResult Edit(Treat treat)
     {
       _db.Treats.Update(treat);
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+    public ActionResult AddFlavor(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor (Treat treat,int flavorId)
+    {
+    #nullable enable
+      FlavorTreat? joinEntity = _db.FlavorTreats.FirstOrDefault(join => (join.FlavorId == flavorId && join.TreatId == treat.TreatId));
+    #nullable disable
+      if(joinEntity == null && flavorId != 0)
+      {
+          _db.FlavorTreats.Add(new FlavorTreat() {FlavorId = flavorId, TreatId = treat.TreatId});
+          _db.SaveChanges();
+      }  
+      return RedirectToAction("Details", new { id = treat.TreatId});
+
     }
   }
 }
