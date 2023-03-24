eb92c548775dc3e75f6a92de3367a639f7141b53
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-24 12:06:03 -0700

    pre lunch commit

--- PierresTreats/Models/Treat.cs
+++ PierresTreats/Models/Treat.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace PierresTreats.Models
 {
-  public class Treat
+  public class Treat 
   {
     public int TreatId { get; set; }
     public string TreatName { get; set; }
