51251f0cbed206f618a62d0464d204952a4114c1
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-24 11:32:54 -0700

    add logic for Treat.cs Flavor.cs PierresTreatsContext.cs and FlavorTreats.cs

---
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PierresTreats.Models
+{
+  public class PierresTreatsContext : DbContext
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<FlavorTreat> FlavorTreats { get; set; }
+
+
+
+    public PierresTreatsContext(DbContextOptions options) : base(options) { }
+  }
+}
